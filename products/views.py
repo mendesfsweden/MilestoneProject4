2aa1dfa1832d428e40cc6b8a2bf5c6c85bd5beed
mendesfsweden <mendesf@hotmail.com>
2020-11-10 23:31:56 +0000

    added detailed product view and url added detailed product template

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -12,3 +12,15 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def detailed_product(request, product_id):
+    """ shows individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/detailed_products.html', context)
