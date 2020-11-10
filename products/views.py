5ba020f951eb8ab61727671ece285a15842857c1
mendesfsweden <mendesf@hotmail.com>
2020-11-10 18:02:55 +0000

    added products view added products url added prodcuts template and some css fixed some css on the header - base template

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def all_products(request):
+    """ shows all products, search bar and sorting """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
