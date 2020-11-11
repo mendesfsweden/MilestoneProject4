c5c3c47454ec757bf7673289a27d93e04ca1b4e8
mendesfsweden <mendesf@hotmail.com>
2020-11-11 14:18:55 +0000

    added btns and its functionality to products template to nav between categories

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, reverse, redirect
-from .models import Product
+from .models import Product, Category
 from django.contrib import messages
 from django.db.models import Q
 
@@ -9,8 +9,13 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    category = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
