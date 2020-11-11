a78038f90bdf5329eb02dade1da36f95281e2c36
mendesfsweden <mendesf@hotmail.com>
2020-11-11 13:45:01 +0000

    added search bar functionality

--- products/views.py
+++ products/views.py
@@ -1,14 +1,28 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse, redirect
 from .models import Product
+from django.contrib import messages
+from django.db.models import Q
 
 
 def all_products(request):
     """ shows all products, search bar and sorting """
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Please enter a word to search for!")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
