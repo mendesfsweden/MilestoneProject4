f9c7458403b8fc923bd696baafaa12f9bfee30c5
mendesfsweden <mendesf@hotmail.com>
2020-11-16 23:10:16 +0000

    removed redundant category model import from products views

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, reverse, redirect
-from .models import Product, Category
+from .models import Product
 from django.contrib import messages
 from django.db.models import Q
 from django.core.paginator import Paginator
