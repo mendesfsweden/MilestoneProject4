f671ceb43ef166759913af1641ce5ac429c98eff
mendesfsweden <mendesf@hotmail.com>
2020-11-25 23:01:46 +0000

    added full_name field to userProfile model

--- profiles/models.py
+++ profiles/models.py
@@ -12,6 +12,8 @@ class UserProfile(models.Model):
     delivery information and order history
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=50,
+                                 null=True, blank=True)
     default_phone_number = models.CharField(max_length=20,
                                             null=True, blank=True)
     default_street_address1 = models.CharField(max_length=80,
