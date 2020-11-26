e1ad112c147a13e0c7ec0e7cd0b97a72fb4589bf
mendesfsweden <mendesf@hotmail.com>
2020-11-26 23:01:16 +0000

    update webhook handler to save profile

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 
 from .models import Order, OrderLineItem
 from products.models import Product
+from profiles.models import UserProfile
 
 import json
 import time
@@ -40,6 +41,19 @@ def handle_payment_intent_succeeded(request, event):
         if value == "":
             shipping_details.address[field] = None
 
+    # Update profile information if save_info was checked
+        username = intent.metadata.username
+        profile = UserProfile.objects.get(user__username=username)
+        if save_info:
+            profile.full_name = shipping_details.name
+            profile.default_phone_number = shipping_details.phone
+            profile.default_country = shipping_details.address.country
+            profile.default_postcode = shipping_details.address.postal_code
+            profile.default_city = shipping_details.address.city
+            profile.default_street_address1 = shipping_details.address.line1
+            profile.default_street_address2 = shipping_details.address.line2
+            profile.save()
+
     order_exists = False
     attempt = 1
     while attempt < 5:
@@ -71,6 +85,7 @@ def handle_payment_intent_succeeded(request, event):
         try:
             order = Order.objects.create(
                 full_name=shipping_details.name,
+                user_profile=profile,
                 email=billing_details.email,
                 phone_number=shipping_details.phone,
                 country=shipping_details.address.country,
