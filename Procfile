35e241a4ff148b5ee22865b67834246140b236bc
mendesfsweden <mendesf@hotmail.com>
2020-11-27 12:25:58 +0000

    added allowed hosts, defined database url variable, created procfile and updated requirements.txt with gunicorn

---
+++ Procfile
@@ -0,0 +1,2 @@
+
+web: gunicorn premiumbody.wsgi:application
--- premiumbody/settings.py
+++ premiumbody/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '7vr-7n*=xcx28u+=ryz9hmj+lrhze&#nzpjfc8#1x!1-#5et@l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['premiumbody.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -118,12 +118,17 @@ WSGI_APPLICATION = 'premiumbody.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
